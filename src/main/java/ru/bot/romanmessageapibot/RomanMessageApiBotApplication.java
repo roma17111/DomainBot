9032b3085222d4046bd39564ec3bee0a65369ab2
roman <romanze1706@gmail.com>
2023-09-04 16:19:53 +0300

    Создан проект

---
+++ src/main/java/ru/bot/romanmessageapibot/RomanMessageApiBotApplication.java
@@ -0,0 +1,13 @@
+package ru.bot.romanmessageapibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanMessageApiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanMessageApiBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bot/romanmessageapibot/RomanMessageApiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bot.romanmessageapibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanMessageApiBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
